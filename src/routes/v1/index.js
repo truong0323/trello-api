f47375de8cbf83b9135e96b8214e7efdfcfcba50
truong <truonglemanh295@gmail.com>
2025-07-16 15:20:10 +0700

    route_learn

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,15 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import express from 'express'
+import {StatusCodes} from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+const Router = express.Router()
+//check api v1/stastus
+Router.get('/status', (req,res)=> {
+    res.status(StatusCodes.OK).json({message: 'APIs V1 are ready to use'
+    })
+
+})
+Router.use( '/boards', boardRoutes)
+export const APIs_V1 = Router
