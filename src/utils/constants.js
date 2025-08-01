c63b89ab9e8e5aef5a553502806630089ee64aa0
truong <truonglemanh295@gmail.com>
2025-08-01 22:21:11 +0700

    Update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,9 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 export const WHITELIST_DOMAINS = [
-    'http://localhost:5173'
+    // 'http://localhost:5173'
+    //vd:75 khoong cần loclahost nũa vì trong config/cors đã luôn luôn cho phép mooi trường:
+    //env.BUILD_MODE === 'dev' 
 
 ]
 export const BOARD_TYPES = {
